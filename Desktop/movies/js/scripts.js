d8cf79ca357472c710499fb8f86a5dc280569d28
curt caldwell and eliot charette <curt@>
2018-06-12 11:57:58 -0700

    Create app that will add calculate and prices based on users selections

---
+++ Desktop/movies/js/scripts.js
@@ -0,0 +1,24 @@
+
+var threeD = 3;
+var reg = 0;
+var type = [threeD, reg];
+
+var youth = 7;
+var student = 8;
+var adult = 10;
+var senior = 20;
+var age = [youth, student, adult, senior];
+
+var mat = 0;
+var night = 2;
+var time = [mat, night];
+
+var prices = [type, age, time];
+
+  console.log(type[1] + age[2] + time[1]);
+
+
+var age = new Price[7,8,10,20]
+function Price(movie, age, time) {
+  this.movie =
+}
